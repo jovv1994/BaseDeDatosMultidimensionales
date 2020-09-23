f2e89c94588510ceaa89d47d6f5f4d48ba6c2b9a
John Orlando Vásconez Varas <38251240+jovv1994@users.noreply.github.com>
2020-09-23 11:40:50 -0500

    Delete jsontocouch.py

--- jsontocouch.py
+++
@@ -1,8 +0,0 @@
-import couchdb
-import json
-couch = couchdb.Server('http://jovv:CouchDB1994@localhost:5984/')
-db = couch['proyectofinal']
-with open("datosfinal.json") as jsonfile:
-    for row in jsonfile:
-        db_entry = json.loads(row)
-        db.save(db_entry)
